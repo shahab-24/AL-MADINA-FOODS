63972f66faaabb3ab5f9b25b0433a4c5b7b60f64
SHAHAB UDDIN <shahab.82946@gmail.com>
2024-12-22 15:38:50 +0600

    footer added

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+	return (
+		<div>
+			<h3>Banner</h3>
+		</div>
+	);
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,9 @@
+import Banner from "../../Components/Banner";
+
 const Home = () => {
   return (
     <div>
+      <Banner></Banner>
       <h3>Home</h3>
     </div>
   );
