433143866e2a3287c7525d21474a91fc32aae2e3
SHAHAB UDDIN <shahab.82946@gmail.com>
2024-12-22 15:34:59 +0600

    navbar added

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar";
 
 
 const HomeLayout = () => {
 	return (
 		<div>
+			<Navbar></Navbar>
 			<Outlet></Outlet>
 		</div>
 	);
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,6 +4,8 @@ import {
   } from "react-router-dom";
 import HomeLayout from "../Layout/HomeLayout";
 import Home from "../Pages/Home/Home";
+import Register from "../Pages/Register/Register";
+import Login from "../Pages/Login/Login";
 
 
   const router = createBrowserRouter([
@@ -14,6 +16,14 @@ import Home from "../Pages/Home/Home";
 		
 		path: '/',
 		element: <Home></Home>
+	  },
+	  {
+		path: "register",
+		element: <Register></Register>
+	  },
+	  {
+		path: 'login',
+		element: <Login></Login>
 	  }
 	  ]
 	},
